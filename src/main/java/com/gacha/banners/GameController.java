8e7a4241a4103d940d10fa4b7673f807528838f7
S_Thar_Htet <40667691@live.napier.ac.uk>
2025-02-20 22:54:37 +0630

    Fix CORS issue for Netlify frontend

--- src/main/java/com/gacha/banners/GameController.java
+++ src/main/java/com/gacha/banners/GameController.java
@@ -10,7 +10,7 @@ import java.io.FileWriter;
 import java.util.*;
 
 //@CrossOrigin(origins = "http://localhost:5173") // allow frontend to access backend; remember this setting
-@CrossOrigin(origins = "https://gacha-banner-calculator.netlify.app")  // allow Netlify frontend
+//@CrossOrigin(origins = "https://gacha-banner-calculator.netlify.app")  // allow Netlify frontend
 @RestController
 @RequestMapping("/games")
 public class GameController {
